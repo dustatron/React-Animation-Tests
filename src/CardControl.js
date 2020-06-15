d79256b829bbfc6c2d1409c7adbacb3cf30dbfdc
Dusty McCord <Dustymccord@gmail.com>
2020-06-15 10:34:35 -0700

    add react-spring and hover animations.

--- src/CardControl.js
+++ src/CardControl.js
@@ -2,14 +2,27 @@ import React from 'react';
 import SampleData from './sampleData';
 import Card from './Card';
 import './scss/_card-control.scss';
+import { animated, useTransition } from 'react-spring';
 
 const CardControl = (props) => {
+  const transition = useTransition(SampleData, (SampleData) => SampleData.id, {
+    from: { opacity: 0, marginLeft: -100, marginRight: 100 },
+    enter: { opacity: 1, marginLeft: 0, marginRight: 0 },
+    trail: 150,
+  }); // returns array, (item, item key, styling)
+
+  console.log('transition', transition);
+
   return (
     <div className='card-controller'>
-      <h2>Cards here</h2>
       <div className='card-container'>
-        {SampleData.map((data) => (
-          <Card title={data.title} img={data.img} cda={data.cda} />
+        {transition.map(({ item, key, props }) => (
+          <animated.div
+            key={key}
+            style={props}
+            className='card-container-animated-box'>
+            <Card title={item.title} img={item.img} cda={item.cda} />
+          </animated.div>
         ))}
       </div>
     </div>
--- src/sampleData.js
+++ src/sampleData.js
@@ -1,30 +1,36 @@
 const SampleData = [
   {
+    id: 1,
     title: 'Long Path',
     cda: 'Take a walk',
     img: require('./media/path.jpg'),
   },
   {
+    id: 2,
     title: 'City Streets',
     cda: 'Make memories',
     img: require('./media/daddydaughter.jpg'),
   },
   {
+    id: 3,
     title: 'Good Times',
     cda: 'Chat with friends',
     img: require('./media/girls.jpeg'),
   },
   {
+    id: 4,
     title: 'Birthday',
     cda: 'Eat cake',
     img: require('./media/birthday.jpg'),
   },
   {
+    id: 5,
     title: 'Picnic times',
     cda: 'Eat out',
     img: require('./media/picnic.jpg'),
   },
   {
+    id: 6,
     title: 'Snuggle buddy',
     cda: 'Pet a dog',
     img: require('./media/pug.jpg'),
