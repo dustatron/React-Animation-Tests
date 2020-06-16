2e31248fc379cef480833e7359932aa7cfe8e5d1
Dusty McCord <Dustymccord@gmail.com>
2020-06-16 16:52:43 -0700

    tweaked styling based on comments.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 import './scss/_body.scss';
 import Navbar from './components/Navbar';
 import CardControl from './components/CardControl';
 import Show from './components/Show';
 import NotFound from './components/NotFound';
 import About from './components/About';
-import { Switch, Route, useLocation, __RouterContext } from 'react-router-dom';
+import {
+  Switch,
+  Route,
+  useLocation,
+  __RouterContext,
+  Redirect,
+} from 'react-router-dom';
 import { useTransition, animated } from 'react-spring';
 
 function App() {
@@ -32,19 +38,26 @@ function App() {
   });
 
   return (
-    <div>
+    <Fragment>
       <Navbar />
       {transitions.map(({ item: location, props, key }) => (
         <animated.div key={key} style={props} className='content'>
           <Switch location={location}>
             <Route exact path='/show/:id' component={Show} />
-            <Route exact path='/' component={CardControl} />
+            <Route exact path='/' component={CardControl}>
+              <Redirect to='/React-Animation-Tests' />
+            </Route>
+            <Route
+              exact
+              path='/React-Animation-Tests'
+              component={CardControl}
+            />
             <Route exact path='/about' component={About} />
             <Route component={NotFound} />
           </Switch>
         </animated.div>
       ))}
-    </div>
+    </Fragment>
   );
 }
 
--- src/components/Show.js
+++ src/components/Show.js
@@ -11,37 +11,35 @@ const Show = ({ match }) => {
   const [grow, setGrow] = useState(false);
 
   return (
-    <div className='show-container'>
-      <div className='show-box'>
-        <div className='show-box-left'>
-          <div className='img-box'>
-            <img src={img} alt='' />
-          </div>
+    <div className='show-box'>
+      <div className='show-box-left'>
+        <div className='img-box'>
+          <img src={img} alt='' />
+          <Link to='/'>
+            <div className='back'> Back </div>
+          </Link>
         </div>
-        <div className='show-box-right'>
-          <h2>{title}</h2>
-          <h3>{cda}</h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
-            eius asperiores amet soluta minima laborum temporibus rem vero
-            magnam quas eaque modi nisi itaque esse adipisci quo, delectus quasi
-            repudiandae! Voluptatum, id veritatis, tempore ipsa reiciendis
-            pariatur porro error, molestias dolor ad ea? Non voluptatibus iusto
-            repudiandae iste possimus reprehenderit et qui ab nam aut harum
-            facilis distinctio nostrum numquam, at, repellendus repellat sit
-            debitis accusantium temporibus odit? Quidem, impedit officia amet
-            dolore ullam placeat iste ratione ipsa similique temporibus
-          </p>
-          <div className={`buy-now `} onClick={() => setGrow(!grow)}>
-            Buy Now
-          </div>
-          <div
-            className={`buy-now-ball ${grow ? ' grow' : ' shrink'}`}
-            onClick={() => setGrow(!grow)}></div>
+      </div>
+      <div className='show-box-right'>
+        <div className='show-box-right-title'>{title}</div>
+        <div className='show-box-right-sub-title'>{cda}</div>
+        <div class='show-box-right-copy'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+          eius asperiores amet soluta minima laborum temporibus rem vero magnam
+          quas eaque modi nisi itaque esse adipisci quo, delectus quasi
+          repudiandae! Voluptatum, id veritatis, tempore ipsa reiciendis
+          pariatur porro error, molestias dolor ad ea? Non voluptatibus iusto
+          repudiandae iste possimus reprehenderit et qui ab nam aut harum
+          facilis distinctio nostrum numquam, at, repellendus repellat sit
+          debitis accusantium temporibus odit? Quidem, impedit officia amet
+          dolore ullam placeat iste ratione ipsa similique temporibus
         </div>
-        <Link to='/'>
-          <div className='back'> Back </div>
-        </Link>
+        <div className={`buy-now`} onClick={() => setGrow(!grow)}>
+          Elise
+        </div>
+        <div
+          className={`buy-now-ball ${grow ? ' grow' : ' shrink'}`}
+          onClick={() => setGrow(!grow)}></div>
       </div>
     </div>
   );
